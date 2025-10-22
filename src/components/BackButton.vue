8e6192c93ad5e6f8e1a96ce8f93aa45ccbcb1999
simonity <simonity3000@gmail.com>
2025-10-22 17:25:55 +0300

    Refactor base button, extract logic from the component

---
+++ src/components/BackButton.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import backIcon from "../assets/icons/back-icon.png";
+</script>
+
+<template>
+  <button class="back-button" aria-label="Back">
+    <img
+      :src="backIcon"
+      alt="Back button"
+      aria-hidden="true"
+      class="back-button__icon"
+    />
+  </button>
+</template>
+
+<style scoped lang="scss">
+.back-button {
+  position: relative;
+  border: none;
+  background: transparent;
+  cursor: pointer;
+
+  &__icon {
+    position: absolute;
+    top: 7px;
+  }
+}
+</style>
