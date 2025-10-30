e866cf3ac5f7b1744c50a6c571e04d648002c9f1
simonity <simonity3000@gmail.com>
2025-10-30 03:37:08 +0200

    Add svgLoader plugin and import SCSS variables/mixins

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'node:path'
+import svgLoader from 'vite-svg-loader';
+
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+  svgLoader()],
   resolve: {
     alias: { '@': path.resolve(__dirname, 'src') },
   },
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@use "@/assets/styles/variables" as *;`,
+        additionalData: `
+        @use "@/assets/styles/variables" as *;
+        @use "@/assets/styles/mixins" as *;
+      `,
       },
     },
   },
