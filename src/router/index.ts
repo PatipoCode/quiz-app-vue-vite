da4f609295e622d72faf57ec06191eeebbd46a25
simonity <simonity3000@gmail.com>
2026-02-04 00:50:26 +0200

    Fix: add base URL to router for GitHub Pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { useQuizStore } from '../store/quiz'
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         { path: '/', name: 'language', component: () => import('../views/LanguageView.vue') },
         { path: '/quiz', name: 'quiz', component: () => import('../views/QuizView.vue') },
