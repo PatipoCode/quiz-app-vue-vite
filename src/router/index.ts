f9f64b6ed6a33e76972f14840e2d2bff0eba0b10
simonity <simonity3000@gmail.com>
2026-02-04 16:54:47 +0200

    Fix page reload on Github Pages with hash

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useQuizStore } from '../store/quiz'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: [
         { path: '/', name: 'language', component: () => import('../views/LanguageView.vue') },
         { path: '/quiz', name: 'quiz', component: () => import('../views/QuizView.vue') },
